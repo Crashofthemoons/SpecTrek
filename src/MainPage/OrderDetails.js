2f1c4d8b3d04052a2c2af4903b076464556d0df7
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-08-08 09:33:08 -0500

    added styling

--- src/MainPage/OrderDetails.js
+++ src/MainPage/OrderDetails.js
@@ -21,27 +21,72 @@ export default class OrderDetails extends Component {
         let mounting
         let verification
         let shipped
+        let dateObj = new Date()
+        let month = dateObj.getMonth() + 1;
+        let day = dateObj.getDate();
+        let year = dateObj.getFullYear();
+        let newDate = month + "/" + day + "/" + year
 
-        if (this.props.order.orderStatus === "Submitted") {
+        let weekOut = new Date()
+        let shipMonth = weekOut.getMonth() +1
+        let shipDay = weekOut.getDate() +1
+        let shipYear = weekOut.getFullYear()
+        let shippingDate = shipMonth + "/" + shipDay + "/" + shipYear
+        console.log(shippingDate)
+
+
+        if (this.props.order.orderStatus === "Checked-In" && this.props.order.remake === true) {
+            checkedIn = "checkedIn-remake"
+        } else if (this.props.order.orderStatus === "Tinting" && this.props.order.remake === true) {
+            tinting = "tinting-remake"
+        } else if (this.props.order.orderStatus === "Blocking" && this.props.order.remake === true) {
+            blocking = 'blocking-remake'
+        } else if (this.props.order.orderStatus === "Edging" && this.props.order.remake === true) {
+            edging = "edging-remake"
+        } else if (this.props.order.orderStatus === "Polishing" && this.props.order.remake === true) {
+            polishing = "polishing-remake"
+        } else if (this.props.order.orderStatus === "Coating" && this.props.order.remake === true) {
+            coating = "coating-remake"
+        } else if (this.props.order.orderStatus === "Mounting" && this.props.order.remake === true) {
+            mounting = "mounting-remake"
+        } else if (this.props.order.orderStatus === "Verification" && this.props.order.remake === true) {
+            verification = "verification-remake"
+        } else if (this.props.order.orderStatus === "Submitted" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             submitted = "submitted"
-        } else if (this.props.order.orderStatus === "Checked-In") {
+        } else if (this.props.order.orderStatus === "Checked-In" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             checkedIn = "checkedIn"
-        } else if (this.props.order.orderStatus === "Tinting") {
+        } else if (this.props.order.orderStatus === "Tinting" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             tinting = "tinting"
-        } else if (this.props.order.orderStatus === "Blocking") {
+        } else if (this.props.order.orderStatus === "Blocking" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             blocking = 'blocking'
-        } else if (this.props.order.orderStatus === "Edging") {
+        } else if (this.props.order.orderStatus === "Edging" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             edging = "edging"
-        } else if (this.props.order.orderStatus === "Polishing") {
+        } else if (this.props.order.orderStatus === "Polishing" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             polishing = "polishing"
-        } else if (this.props.order.orderStatus === "Coating") {
+        } else if (this.props.order.orderStatus === "Coating" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             coating = "coating"
-        } else if (this.props.order.orderStatus === "Mounting") {
+        } else if (this.props.order.orderStatus === "Mounting" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             mounting = "mounting"
-        } else if (this.props.order.orderStatus === "Verification") {
+        } else if (this.props.order.orderStatus === "Verification" && parseInt(this.props.order.shipDate) >= parseInt(newDate)) {
             verification = "verification"
         } else if (this.props.order.orderStatus === "Shipped") {
             shipped = "shipped"
+        } else if (this.props.order.orderStatus === "Checked-In") {
+            checkedIn = "checkedIn-late"
+        } else if (this.props.order.orderStatus === "Tinting") {
+            tinting = "tinting-late"
+        } else if (this.props.order.orderStatus === "Blocking") {
+            blocking = 'blocking-late'
+        } else if (this.props.order.orderStatus === "Edging") {
+            edging = "edging-late"
+        } else if (this.props.order.orderStatus === "Polishing") {
+            polishing = "polishing-late"
+        } else if (this.props.order.orderStatus === "Coating") {
+            coating = "coating-late"
+        } else if (this.props.order.orderStatus === "Mounting") {
+            mounting = "mounting-late"
+        } else if (this.props.order.orderStatus === "Verification") {
+            verification = "verification-late"
         }
         return (
             <React.Fragment>
